44a254457f178e687b7963434581f2a4efa21768
Pmangmoney1 <gewen0620@gmail.com>
2026-05-09 19:12:31 +0800

    Update auth.js

--- functions/api/auth.js
+++ functions/api/auth.js
@@ -1,10 +1,7 @@
 export async function onRequestGet(context) {
   const { env } = context;
-  const client_id = env.GITHUB_CLIENT_ID;
-  
-  // 깃허브 승인 페이지로 리다이렉트
   return Response.redirect(
-    `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo,user`,
+    `https://github.com/login/oauth/authorize?client_id=${env.GITHUB_CLIENT_ID}&scope=repo,user`,
     302
   );
-} // <- 이 닫는 괄호가 반드시 있어야 합니다.
+}
