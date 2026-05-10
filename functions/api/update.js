8828a797288e7207954fd49ec268c1fe45663c9c
Pmangmoney1 <gewen0620@gmail.com>
2026-05-11 07:44:53 +0800

    Update update.js

--- functions/api/update.js
+++ functions/api/update.js
@@ -1,3 +1,32 @@
-export async function onRequest(context) {
-  return new Response("Update endpoint working!");
+export async function onRequestPost(context) {
+
+  try {
+
+    const data = await context.request.json();
+
+    return new Response(
+      JSON.stringify({
+        success: true,
+        data
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+  } catch (e) {
+
+    return new Response(
+      JSON.stringify({
+        success: false
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+  }
 }
