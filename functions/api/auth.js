a0d444a568b8612d349c3ad94323dd1265abdad7
Pmangmoney1 <gewen0620@gmail.com>
2026-05-10 13:36:16 +0800

    Delete functions/api/auth.js

--- functions/api/auth.js
+++
@@ -1,7 +0,0 @@
-export async function onRequestGet(context) {
-  const { env } = context;
-  return Response.redirect(
-    `https://github.com/login/oauth/authorize?client_id=${env.GITHUB_CLIENT_ID}&scope=repo,user`,
-    302
-  );
-}
