bc93f42b5f3bccd89eec6d3375576e82afae3972
Pmangmoney1 <gewen0620@gmail.com>
2026-05-11 12:48:20 +0800

    Update images.js

--- functions/api/images.js
+++ functions/api/images.js
@@ -1,7 +1,30 @@
-export async function onRequestGet() {
+export async function onRequestGet(context) {
 
-  return Response.json([
-    "https://res.cloudinary.com/dxnztulzc/image/upload/v1778465635/photo_2026-04-27_16-45-20_dwpdhz.jpg"
-  ]);
+  const cloudName =
+    context.env.CLOUDINARY_CLOUD_NAME;
+
+  const apiKey =
+    context.env.CLOUDINARY_API_KEY;
+
+  const apiSecret =
+    context.env.CLOUDINARY_API_SECRET;
+
+  const auth =
+    btoa(`${apiKey}:${apiSecret}`);
+
+  const res = await fetch(
+    `https://api.cloudinary.com/v1_1/${cloudName}/resources/image`,
+    {
+      headers:{
+        Authorization:`Basic ${auth}`
+      }
+    }
+  );
+
+  const data = await res.json();
+
+  return Response.json(
+    data.resources.map(img=>img.secure_url)
+  );
 
 }
